0e0b65bf7907a52c691bb0b0b74606bae0cf73f0
Lucas Borgesgit <lucasborgesmoraes@gmail.com>
2024-08-02 02:42:52 -0300

    hate a-frame

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ let count = 'Click para ter uma resposta';
  */
 function onClick() {
     console.log('Click Event Occurred');
-    count = 'Você clicou e eu respondi';
+    const randomNumber = Math.random();
+    if (randomNumber < 0.25) {
+        count = 'Sim';
+    } else if (randomNumber < 0.5) {
+        count = 'Nao';
+    } else if (randomNumber < 0.75) {
+        count = 'Talvez';
+    } else {
+        count = 'Improvavel';
+    }
     var textElement = document.querySelector('#animated-marker a-text');
     textElement.setAttribute('value', count);
 }
 
 // Call the onClick function when a click event occurs
 document.addEventListener('click', onClick);
+
